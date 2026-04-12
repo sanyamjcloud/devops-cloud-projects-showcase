9d1312173801369cacb30cd79726f12415416538
Sanyam Jain <sanyamj.cloud@gmail.com>
2026-04-12 23:40:23 +0530

    Update self_heal.sh

--- self-healing-docker-nginx-setup/self_heal.sh
+++ self-healing-docker-nginx-setup/self_heal.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 if ! pgrep nginx > /dev/null; then
-    echo "[$TIMESTAMP] NGINX not running! Restarting..."
+    echo "[$TIMESTAMP] NGINX not running. Restarting..."
     service nginx start
     echo "[$TIMESTAMP] NGINX restart exit code: $?"
 else
